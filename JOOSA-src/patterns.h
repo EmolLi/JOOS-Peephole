b13d1e5360d47fb0ef6f7d399d4d577f43b72f6c
EmolLi <duan.li@mail.mcgill.ca>
2018-04-08 02:22:30 -0400

    ADD: pattern simplify_cmp_goto 14599/16649

--- JOOSA-src/patterns.h
+++ JOOSA-src/patterns.h
@@ -88,6 +88,64 @@ int positive_increment(CODE **c)
   return 0;
 }
 
+/* iload x
+ * ldc k   (0<=k<=128)
+ * isub
+ * istore x
+ * --------->
+ * iinc x -k
+ *//*
+int positive_decrement(CODE **c)
+{ int x,y,k;
+  if (is_iload(*c,&x) &&
+      is_ldc_int(next(*c),&k) &&
+      is_isub(next(next(*c))) &&
+      is_istore(next(next(next(*c))),&y) &&
+      x==y && 0<=k && k<=128) {
+     return replace(c,4,makeCODEiinc(x,-k,NULL));
+  }
+  return 0;
+}*/
+
+
+
+
+/*
+   if_icmpX l1
+   ldc a (a = 0)
+   goto l2
+   l1:
+   ldc b (b = 1)
+   l2:
+   ifeq l3
+   ----->
+   oppositeOfIf_icmpX l3    e.g if if_icmpX is is_if_icmplt, then oppositeOfIf_icmpX should be is_if_icmpge
+   (l1, l2 reference count reduced by 1)
+ */
+ int simplify_cmp_goto(CODE **c)
+ { int l1, l1t, l2, l2t, l3;
+   int a, b;
+   if ( (is_if_icmplt(*c, &l1) || is_if_icmpeq(*c, &l1) || is_if_icmpge(*c, &l1) || is_if_icmpgt(*c, &l1) || is_if_icmple(*c, &l1) || is_if_icmpne(*c, &l1)) &&
+        is_ldc_int(next(*c), &a) && a == 0 &&
+        is_goto(next(next(*c)), &l2) &&
+        is_label(next(next(next(*c))), &l1t) && l1t == l1 &&
+        is_ldc_int(next(next(next(next(*c)))), &b) && b == 1 &&
+        is_label(next(next(next(next(next(*c))))), &l2t) && l2t == l2 &&
+        is_ifeq(next(next(next(next(next(next(*c)))))), &l3)) {
+            droplabel(l1);
+            droplabel(l2);
+            if (is_if_icmplt(*c, &l1)) return replace(c,7,makeCODEif_icmpge(l3,NULL));
+            if (is_if_icmpge(*c, &l1)) return replace(c,7,makeCODEif_icmplt(l3,NULL));
+            if (is_if_icmpeq(*c, &l1)) return replace(c,7,makeCODEif_icmpne(l3,NULL));
+            if (is_if_icmpne(*c, &l1)) return replace(c,7,makeCODEif_icmpeq(l3,NULL));
+            if (is_if_icmpgt(*c, &l1)) return replace(c,7,makeCODEif_icmple(l3,NULL));
+            if (is_if_icmple(*c, &l1)) return replace(c,7,makeCODEif_icmpgt(l3,NULL));
+   }
+   return 0;
+ }
+
+
+
 /* goto L1
  * ...
  * L1:
@@ -117,4 +175,5 @@ void init_patterns(void) {
 	ADD_PATTERN(simplify_istore);
 	ADD_PATTERN(positive_increment);
 	ADD_PATTERN(simplify_goto_goto);
+	ADD_PATTERN(simplify_cmp_goto);
 }
